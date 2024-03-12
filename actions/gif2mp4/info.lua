3b0eb0bcae504d82438511e5728181b12bc98dd0
盒子 <i@box.gd>
2024-03-12 22:33:29 +0800

    使用声明文件作为动作标题

---
+++ actions/gif2mp4/info.lua
@@ -0,0 +1,8 @@
+local config = {
+	name = "GIF to MP4",
+	mime = {
+		"image/gif",
+	}
+}
+
+return config
---
+++ actions/rename_to_hash/info.lua
@@ -0,0 +1,8 @@
+local config = {
+	name = "Rename to Hash",
+	mime = {
+		"image/gif",
+	}
+}
+
+return config
